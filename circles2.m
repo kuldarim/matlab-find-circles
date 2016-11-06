703a9c07c236c1c3a871b3247fa43946651575a3
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-11-06 04:20:42 -0800

    almost finished

--- circles2.m
+++ circles2.m
@@ -15,17 +15,11 @@ bw = imclose(bw,se);
 
 [B,L] = bwboundaries(bw,'noholes');
 
-% Display the label matrix and draw each boundary
-imshow(label2rgb(L, @jet, [.5 .5 .5]))
-hold on
-for k = 1:length(B)
-  boundary = B{k};
-  plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 2)
-end
-
 stats = regionprops(L,'Area','Centroid');
-
 threshold = 0.82;
+
+% Display the label matrix and draw each boundary
+hold on
 
 % loop over the boundaries
 for k = 1:length(B)
@@ -49,8 +43,7 @@ for k = 1:length(B)
   % mark objects above the threshold with a black circle
   if metric > threshold
     centroid = stats(k).Centroid;
-    plot(centroid(1),centroid(2),'ko');
-    
+    plot(boundary(:,2), boundary(:,1), 'w', 'LineWidth', 2)
     text(boundary(1,2)-35,boundary(1,1)+13,metric_string,'Color','y',...
        'FontSize',14,'FontWeight','bold');
   end
